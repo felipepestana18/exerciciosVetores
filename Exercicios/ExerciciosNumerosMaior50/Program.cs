aeb73c2ad76b767f735ab01150d1f712adf0c3b1
felipepestana <felipe-pe27@hotmail.com>
2023-08-30 12:51:51 -0300

    add: exercicio 2 para vérificar os numeros maiores que 50

---
+++ Exercicios/ExerciciosNumerosMaior50/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] numeros = new int[10];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("Digite o numéro: ");
+    numeros[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+
+for (int i = 0; i < numeros.Length; i++)
+{
+
+    if (Convert.ToInt32(numeros[i]) > 50 ) {
+        Console.WriteLine("O numéro é maior que o 50" + numeros[i] + "está na posicição" + i);
+    } else if (numeros[i]  <= 0)
+    {
+        Console.WriteLine("Não existe nenhum valor na posição: " + i);
+    }
+
+}
+
