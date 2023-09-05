8163ca6d6c3de42fc783c014e80b3420b25cf16f
felipepestana <felipe-pe27@hotmail.com>
2023-09-05 18:30:08 -0300

    Exercico 4 - Vetor Inverso

---
+++ Exercicios/ExercicioVetorInversa/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int [] vetorNumeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Digite 5 numeros");
+    vetorNumeros[i] = Convert.ToInt32(Console.ReadLine());  
+}
+
+Console.WriteLine("Digite o Código");
+int codigo = Convert.ToInt32(Console.ReadLine());  
+
+if (codigo != 0)
+{
+    if(codigo == 1)
+    {
+        for (int i = 0; i < vetorNumeros.Length; i++)
+        {
+            Console.WriteLine(vetorNumeros[i]);
+        }
+    }
+
+    if (codigo == 2)
+    {
+        Console.WriteLine("Numeros Inversos");
+        for (int i = vetorNumeros.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(vetorNumeros[i]);
+        }
+    }
+
+}
